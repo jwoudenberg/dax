7a79cf3b600599022466536194ec5a3c24b35b63
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-04-20 22:20:34 +0100

    Interpret the API as a Scotty application
    By interpreting our API type as a Scotty application we can run it as an
    actual web server. This approach saves the work of implementing things
    like routing logic because Scotty takes care of all that. This seems the
    quickest way to bootstrap the application.

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,51 +6,55 @@ module Lib
   ( someFunc
   ) where
 
+import "base" Data.Foldable (traverse_)
 import "text" Data.Text (Text)
 import "text" Data.Text.Lazy (fromStrict)
-import "wai" Network.Wai
+import "scotty" Web.Scotty
 
 import qualified "aeson" Data.Aeson as Aeson
+import qualified "text" Data.Text as Text
 import qualified "http-types" Network.HTTP.Types.Status as Status
-import qualified "scotty" Web.Scotty as Scotty
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data Route a where
   Get :: (Aeson.ToJSON b) => Route b
-  StaticParam :: Text -> Route b -> Route b
-  Param :: (Scotty.Parsable a) => Text -> Route b -> Route (a -> b)
+  PathSegmentStatic :: Text -> Route b -> Route b
+  PathSegmentCapture :: (Parsable a) => Text -> Route b -> Route (a -> b)
 
 data Endpoint where
   Endpoint :: Route a -> a -> Endpoint
 
 type API = [Endpoint]
 
--- Naive serve function
-serve :: API -> Request -> Response
-serve endpoints req =
-  case route req endpoints of
-    Just endpoint -> serveEndpoint endpoint (RequestInfo (pathInfo req))
-    Nothing -> responseLBS Status.status404 [] ""
+-- |
+-- Interpret the API as a Scotty application.
+scotty :: API -> ScottyM ()
+scotty = traverse_ serveEndpoint
 
-data RequestInfo = RequestInfo
-  { path :: [Text]
-  }
+serveEndpoint :: Endpoint -> ScottyM ()
+serveEndpoint (Endpoint route handler) = serveEndpoint' End route (pure handler)
 
-route :: Request -> API -> Maybe Endpoint
-route = undefined
+serveEndpoint' :: Path -> Route a -> ActionM a -> ScottyM ()
+serveEndpoint' path route f =
+  case route of
+    Get -> get (toPath path) (json =<< f)
+    (PathSegmentStatic name sub) -> serveEndpoint' (Static name path) sub f
+    (PathSegmentCapture name sub) ->
+      serveEndpoint' (Capture name path) sub (f <*> param (fromStrict name))
 
-serveEndpoint :: Endpoint -> RequestInfo -> Response
-serveEndpoint (Endpoint route x) req = serveRoute route x req
+data Path
+  = End
+  | Static Text
+           Path
+  | Capture Text
+            Path
 
-serveRoute :: Route a -> a -> RequestInfo -> Response
-serveRoute Get x _ = responseLBS Status.status200 [] (Aeson.encode x)
-serveRoute (StaticParam _ sub) x req =
-  serveRoute sub x req {path = tail (path req)}
-serveRoute (Param _ sub) f req =
-  let newPath = tail (path req)
-      param' = Scotty.parseParam . fromStrict . head $ path req
-   in case param' of
-        Right param -> serveRoute sub (f param) req {path = newPath}
-        Left _ -> responseLBS Status.status400 [] ""
+toPath :: Path -> RoutePattern
+toPath = capture . Text.unpack . toPath' ""
+
+toPath' :: Text -> Path -> Text
+toPath' acc End = acc
+toPath' acc (Static segment rest) = toPath' ("/" <> segment <> acc) rest
+toPath' acc (Capture name rest) = toPath' ("/:" <> name <> acc) rest
