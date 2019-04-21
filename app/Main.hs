7bb721f466c3de125bdce8358d743f9590cb9ea6
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-04-21 18:26:55 +0100

    Experiment by writing example server differently
    The experiment is to find out which approach produces the nicest error
    messages. The convention used here of specifying the route and handler
    separately looks to produce slightly nicer error messages if they do not
    have the same time.

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,11 @@ import Lib
 import qualified "warp" Network.Wai.Handler.Warp as Warp
 
 main :: IO ()
-main = Warp.run 5000 =<< application [getKelvin]
+main = Warp.run 5000 =<< application [endpoint toKelvinRoute toKelvin]
 
-getKelvin :: Endpoint
-getKelvin =
-  endpoint
-    (static "centigrade" $ capture "temperature" $ static "kelvin" $ get)
-    centigradeToKelvin
+toKelvinRoute :: Route (Float -> Float)
+toKelvinRoute =
+  static "centigrade" $ capture "temperature" $ static "kelvin" $ get
 
-centigradeToKelvin :: Float -> Float
-centigradeToKelvin = (+) 273.15
+toKelvin :: Float -> Float
+toKelvin = (+) 273.15
