b87326ece58d81f3c722cbd33a156046c234c7c9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-05-09 22:02:00 +0100

    Add outlines for more test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,20 +8,34 @@ import Dax
 import qualified Dax.Response.Json
 import Network.Wai.Test
   ( Session
+  , assertBody
   , assertStatus
   , defaultRequest
   , request
   , runSession
   , setPath
   )
-import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (testCase)
 
 main :: IO ()
 main =
   defaultMain $
   testGroup
-    "Dax.sandbox"
+    "Dax"
+    [ routing
+    , contentDecoding
+    , contentEncoding
+    , effectfulApis
+    , requestHeaders
+    , responseHeaders
+    , docGeneration
+    ]
+
+routing :: TestTree
+routing =
+  testGroup
+    "Routing"
     [ testCase "Request to static route with multiple segments succeeds" $
       runSandbox staticRoute $ do
         response <- request $ setPath defaultRequest "/teas/green/lemon"
@@ -30,7 +44,32 @@ main =
       runSandbox staticRoute $ do
         response <- request $ setPath defaultRequest "/non-existing/route"
         assertStatus 404 response
+    , testCase
+        "Handler for route with multiple capture segments receives arguments in right order" $
+      runSandbox subtractRoute $ do
+        response <- request $ setPath defaultRequest "/from/6/subtract/4"
+        assertStatus 200 response
+        assertBody "2" response
+    -- Request with unsupported method fails with 405
     ]
+
+contentDecoding :: TestTree
+contentDecoding = testGroup "Content decoding" []
+
+contentEncoding :: TestTree
+contentEncoding = testGroup "Content encoding" []
+
+effectfulApis :: TestTree
+effectfulApis = testGroup "Effectful APIs" []
+
+requestHeaders :: TestTree
+requestHeaders = testGroup "Request headers" []
+
+responseHeaders :: TestTree
+responseHeaders = testGroup "Response headers" []
+
+docGeneration :: TestTree
+docGeneration = testGroup "Documentation generation" []
 
 runSandbox :: API NoEffects -> Session () -> IO ()
 runSandbox api assertion = sandbox api >>= runSession assertion
@@ -42,5 +81,17 @@ staticRoute =
       42
   ]
 
+subtractRoute :: API NoEffects
+subtractRoute =
+  [ endpoint
+      (static "from" $
+       capture intDecoder $
+       static "subtract" $ capture intDecoder $ get [intEncoder])
+      (-)
+  ]
+
 intEncoder :: ResponseEncoder Int
 intEncoder = Dax.Response.Json.succeeds
+
+intDecoder :: ParamDecoder Int
+intDecoder = autoParamDecoder
